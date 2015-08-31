d539522f06ad6ec7b2b0a2ee3ec23e426be1409f
Chris Holden <ceholden@gmail.com>
2015-08-31 11:34:29 -0400

    Add WIP on enet/enetCV similar to sklearn

--- glmnet/elastic_net.py
+++ glmnet/elastic_net.py
@@ -2,26 +2,72 @@ import numpy as np
 
 from .glmnet import elastic_net
 
+
 class ElasticNet(object):
-    """ElasticNet based on GLMNET"""
-    def __init__(self, alpha, rho=0.2):
+    """ ElasticNet based on GLMNET
+
+    Args:
+        alpha (float): ElasticNet mixing parameter (0 <= alpha <= 1.0)
+            specifying the mix of Ridge L2 (alpha=0) to Lasso L1 (alpha=1)
+            regularization.
+        lambdas (iterable, or None): Constant that controls the degree of
+            regularization. If None are specified, `n_lambdas` number of
+            lambdas are set automatically using `_alpha_grid` from scikit-learn
+        n_lambdas (int or None): If ``lambdas`` is None, ```n_lambdas`` controls
+            the number of automatically calculated ``lambdas``
+
+    """
+    def __init__(self, alpha=0.5, lambdas=None, n_lambdas=1):
         super(ElasticNet, self).__init__()
         self.alpha = alpha
-        self.rho = rho
+        self.lambdas = lambdas
+        self.n_lambdas = n_lambdas
+
         self.coef_ = None
         self.rsquared_ = None
 
-    def fit(self, X, y):
-        n_lambdas, intercept_, coef_, _, _, rsquared_, lambdas, _, jerr \
-        = elastic_net(X, y, self.rho, lambdas=[self.alpha])
-        # elastic_net will fire exception instead
-        # assert jerr == 0
-        self.coef_ = coef_
+    def fit(self, X, y, weights=None):
+        """ Fit a model predicting y independent variable from X design matrix
+
+        Args:
+            X (np.ndarray): 2D design matrix
+            y (np.ndarray): 1D independent variable
+            weights (np.ndarray): 1D array of weights for each observation in
+                `y`. If None, all observations are weighted equally
+
+        Returns:
+            ElasticNet: return `self` with model results stored for method
+                chaining
+
+        """
+        self.weights = weights or np.ones(y.shape[0])
+
+        # Compute lambdas if necessary
+        if lambdas is None:
+            raise NotImplementedError('Have not added auto-calc of lambdas yet')
+
+        # Fit elastic net
+        n_lambdas, intercept_, coef_, ia, nin, rsquared_, lambdas, _, jerr \
+            = elastic_net(X, y, self.alpha,
+                          lambdas=self.lambdas,
+                          weights=weights)
+
+        # ia is 1 indexed
+        ia = np.trim_zeros(ia, 'b') - 1
+
+        # glmnet.f returns coefficients as 'compressed' array that requires
+        # re-indexing using ia and nin
+        self.coef_ = np.zeros_like(coef_)
+        self.coef_[ia, :] = coef_[:np.max(nin), :n_lambdas]
         self.intercept_ = intercept_
         self.rsquared_ = rsquared_
+
+        # TODO: Calculate "best" alpha for prediction based on MSE
+
         return self
 
     def predict(self, X):
+        # TODO: predict for best model
         return np.dot(X, self.coef_) + self.intercept_
 
     def __str__(self):
@@ -35,7 +81,7 @@ class ElasticNet(object):
 
 
 def elastic_net_path(X, y, rho, **kwargs):
-    """return full path for ElasticNet"""
+    """Return full path for ElasticNet"""
     n_lambdas, intercepts, coefs, _, _, _, lambdas, _, jerr \
     = elastic_net(X, y, rho, **kwargs)
     return lambdas, coefs, intercepts

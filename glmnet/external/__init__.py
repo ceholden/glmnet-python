93445da90b2b84adb9eaf03d62436b45b15b5b9b
Chris Holden <ceholden@gmail.com>
2015-09-04 17:29:32 -0400

    Add external copy of six.py for py2/3

---
+++ glmnet/external/__init__.py
@@ -0,0 +1,8 @@
+""" Code from other projects included here for convenience
+
+six.py
+    - Copyright Benjamin Peterson 2010-2013
+    - License: MIT
+    - PyPI: https://pypi.python.org/pypi/six
+
+"""
